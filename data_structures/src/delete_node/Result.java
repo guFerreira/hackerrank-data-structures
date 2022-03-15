b7ff18154747fe92975575c970c727c481248621
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-15 12:37:16 -0300

    Reverse linked list

--- data_structures/src/delete_node/Result.java
+++ data_structures/src/delete_node/Result.java
@@ -33,20 +33,6 @@ class SinglyLinkedList {
     }
 }
 
-class SinglyLinkedListPrintHelper {
-    public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
-        while (node != null) {
-            bufferedWriter.write(String.valueOf(node.data));
-
-            node = node.next;
-
-            if (node != null) {
-                bufferedWriter.write(sep);
-            }
-        }
-    }
-}
-
 class Result {
 
     /*
---
+++ data_structures/src/reverse_linked_list/Solution.java
@@ -0,0 +1,72 @@
+package reverse_linked_list;
+
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+
+    /*
+     * Complete the 'reverse' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    public static SinglyLinkedListNode reverse(SinglyLinkedListNode llist) {
+        // Write your code here
+        SinglyLinkedListNode atual = llist;
+        SinglyLinkedListNode anterior = null;
+        SinglyLinkedListNode proximo = null;
+
+        while (atual != null) {
+            proximo = atual.next;
+            atual.next = anterior;
+            anterior = atual;
+            atual = proximo;
+        }
+        llist = anterior;
+        return llist;
+
+    }
+}
